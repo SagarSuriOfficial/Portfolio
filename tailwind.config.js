166a5c0489aec3efa69a809ae2f0e10549470e49
SagarSuriOfficial <sagarsuriofficial@gmail.com>
2024-01-24 18:53:28 +0530

    the new file added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./dist/*.html" , "./Projects/food-delivery/index.html" , "./Projects/pwskillsclone/index.html"],
+  content: ["./index.html", "./dist/*.html" , "./Projects/food-delivery/index.html" , "./Projects/pwskillsclone/index.html" , "./Projects/Multi-Page/index.html"],
   theme: {
     extend: {},
   },
