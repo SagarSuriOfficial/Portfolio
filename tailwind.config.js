f5ff86d1d7aa74679afbcbf8532f070c6d26e006
SagarSuriOfficial <sagarsuriofficial@gmail.com>
2024-01-24 22:05:57 +0530

    amazon clone added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./dist/*.html" , "./Projects/food-delivery/index.html" , "./Projects/pwskillsclone/index.html" , "./Projects/Multi-Page/index.html"],
+  content: ["./index.html", "./dist/*.html" , "./Projects/food-delivery/index.html" , "./Projects/pwskillsclone/index.html" , "./Projects/Multi-Page/index.html", "./Projects/AmazonClone/index.html"],
   theme: {
     extend: {},
   },
